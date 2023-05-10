46891d5987dd60145a0e4d9acf084529b0cc4c39
peitianyu <1216582461@qq.com>
2023-05-10 14:44:09 +0800

    测试模块, 添加只测试单测试文件的功能

--- lv_math/src/core/math/common/tt_test.h
+++ lv_math/src/core/math/common/tt_test.h
@@ -43,39 +43,52 @@ public:
 
 	void Register(const std::string &name, void (*test)()){
 		if(!m_tests){ m_tests = new std::vector<ContextTest>();}
-		m_tests->push_back(ContextTest(name, test));
+		m_tests->push_back(ContextTest{name, test});
 	}
 
-	bool RunAllTests(){
+	bool RunAllTests(){		
+		if(m_run_name) return JustRun();
+
 		std::cout << "\033[32m" << "[==========] Running " << m_tests->size() << " tests." << "\033[0m" << std::endl;
-		for(auto &test : *m_tests){
-			std::cout << "\033[32m" << "[ RUN      ] " << test.name << "\033[0m" << std::endl;
-			uint start_time = GetTime();
-			test.test();
-			std::cout << "\033[32m" << "[      OK  ] " << test.name  << " (" << (GetTime() - start_time) << " ms)" << "\033[0m" << std::endl;
-		}
+		for(auto &test : *m_tests)RunTest(test);
 		std::cout << "\033[32m" << "[ ALL TESTS PASSED SUCCESS ]" << "\033[0m" << std::endl;
 		return true;
 	}
+
+	std::string *m_run_name = nullptr;
 private:
 	struct ContextTest{
 		std::string name;
 		void (*test)();
-		
-		ContextTest(const std::string &name, void (*test)()) : name(name), test(test) {}
 	};
 
-	uint GetTime()
-	{
+	uint GetTime(){
 		struct timeval time;
 		gettimeofday(&time, NULL);
 		return time.tv_sec * 1000 + time.tv_usec / 1000;
+	}
+
+	void RunTest(const ContextTest& test){
+		std::cout << "\033[32m" << "[ RUN      ] " << test.name << "\033[0m" << std::endl;
+		uint start_time = GetTime();
+		test.test();
+		std::cout << "\033[32m" << "[      OK  ] " << test.name  << " (" << (GetTime() - start_time) << " ms)" << "\033[0m" << std::endl;
+		std::cout << "\033[32m" << "[-----------------------------------------------------------------------]" << "\033[0m" << std::endl;
+	}
+
+	bool JustRun()
+	{
+		for(auto &test : *m_tests) if(test.name == *m_run_name){RunTest(test); return true;}
+		std::cout << "\033[31m" << "[  ERROR   ] " << "Can't find test: " << *m_run_name << "\033[0m" << std::endl;
+		return false;
 	}
 
 	std::vector<ContextTest> *m_tests;
 };
 
 #ifdef TEST_ENABLE
+
+#define JUST_RUN_TEST(base, name)  struct just##name##_Test{ just##name##_Test() {  RegisterTest::GetInstance()->m_run_name = new std::string(#base "." #name); } }; just##name##_Test g_just##name##_Test;
 #define TEST(base, name)                              											\
 	struct base##name##_Test                          											\
 	{                                                 											\
@@ -88,11 +101,9 @@ private:
 	void base##name##_Test::Run()
 
 #define RunAllTests() RegisterTest::GetInstance()->RunAllTests()
-
 #else
 #define TEST(base, name) void base##name##_Test()
 #define RunAllTests() false
 #endif // TEST_ENABLE
-
 
 #endif // __COMMON_TEST_H__
